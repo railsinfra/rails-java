1b9fdf6df2ced23814801914074fa2844ef7c316
Sibabale <sibabale.joja@oldmutual.com>
2026-04-17 11:27:11 +0200

    chore(rails-java): track generated example build outputs

---
+++ rails-java-core/bin/main/com/rails/api/client/RailsClientImpl.kt
@@ -0,0 +1,75 @@
+// File generated from our OpenAPI spec by Stainless.
+
+package com.rails.api.client
+
+import com.rails.api.core.ClientOptions
+import com.rails.api.core.getPackageVersion
+import com.rails.api.services.blocking.AccountService
+import com.rails.api.services.blocking.AccountServiceImpl
+import com.rails.api.services.blocking.TransactionService
+import com.rails.api.services.blocking.TransactionServiceImpl
+import java.util.function.Consumer
+
+class RailsClientImpl(private val clientOptions: ClientOptions) : RailsClient {
+
+    private val clientOptionsWithUserAgent =
+        if (clientOptions.headers.names().contains("User-Agent")) clientOptions
+        else
+            clientOptions
+                .toBuilder()
+                .putHeader("User-Agent", "${javaClass.simpleName}/Java ${getPackageVersion()}")
+                .build()
+
+    // Pass the original clientOptions so that this client sets its own User-Agent.
+    private val async: RailsClientAsync by lazy { RailsClientAsyncImpl(clientOptions) }
+
+    private val withRawResponse: RailsClient.WithRawResponse by lazy {
+        WithRawResponseImpl(clientOptions)
+    }
+
+    private val accounts: AccountService by lazy { AccountServiceImpl(clientOptionsWithUserAgent) }
+
+    private val transactions: TransactionService by lazy {
+        TransactionServiceImpl(clientOptionsWithUserAgent)
+    }
+
+    override fun async(): RailsClientAsync = async
+
+    override fun withRawResponse(): RailsClient.WithRawResponse = withRawResponse
+
+    override fun withOptions(modifier: Consumer<ClientOptions.Builder>): RailsClient =
+        RailsClientImpl(clientOptions.toBuilder().apply(modifier::accept).build())
+
+    /** Accounts */
+    override fun accounts(): AccountService = accounts
+
+    /** Transactions */
+    override fun transactions(): TransactionService = transactions
+
+    override fun close() = clientOptions.close()
+
+    class WithRawResponseImpl internal constructor(private val clientOptions: ClientOptions) :
+        RailsClient.WithRawResponse {
+
+        private val accounts: AccountService.WithRawResponse by lazy {
+            AccountServiceImpl.WithRawResponseImpl(clientOptions)
+        }
+
+        private val transactions: TransactionService.WithRawResponse by lazy {
+            TransactionServiceImpl.WithRawResponseImpl(clientOptions)
+        }
+
+        override fun withOptions(
+            modifier: Consumer<ClientOptions.Builder>
+        ): RailsClient.WithRawResponse =
+            RailsClientImpl.WithRawResponseImpl(
+                clientOptions.toBuilder().apply(modifier::accept).build()
+            )
+
+        /** Accounts */
+        override fun accounts(): AccountService.WithRawResponse = accounts
+
+        /** Transactions */
+        override fun transactions(): TransactionService.WithRawResponse = transactions
+    }
+}
---
+++ rails-java-core/bin/main/com/rails/api/services/async/TransactionServiceAsyncImpl.kt
@@ -0,0 +1,171 @@
+// File generated from our OpenAPI spec by Stainless.
+
+package com.rails.api.services.async
+
+import com.rails.api.core.ClientOptions
+import com.rails.api.core.RequestOptions
+import com.rails.api.core.checkRequired
+import com.rails.api.core.handlers.errorBodyHandler
+import com.rails.api.core.handlers.errorHandler
+import com.rails.api.core.handlers.jsonHandler
+import com.rails.api.core.http.HttpMethod
+import com.rails.api.core.http.HttpRequest
+import com.rails.api.core.http.HttpResponse
+import com.rails.api.core.http.HttpResponse.Handler
+import com.rails.api.core.http.HttpResponseFor
+import com.rails.api.core.http.parseable
+import com.rails.api.core.prepareAsync
+import com.rails.api.models.transactions.TransactionListByAccountParams
+import com.rails.api.models.transactions.TransactionListByAccountResponse
+import com.rails.api.models.transactions.TransactionListParams
+import com.rails.api.models.transactions.TransactionListResponse
+import com.rails.api.models.transactions.TransactionRetrieveParams
+import com.rails.api.models.transactions.TransactionRetrieveResponse
+import java.util.concurrent.CompletableFuture
+import java.util.function.Consumer
+import kotlin.jvm.optionals.getOrNull
+
+/** Transactions */
+class TransactionServiceAsyncImpl internal constructor(private val clientOptions: ClientOptions) :
+    TransactionServiceAsync {
+
+    private val withRawResponse: TransactionServiceAsync.WithRawResponse by lazy {
+        WithRawResponseImpl(clientOptions)
+    }
+
+    override fun withRawResponse(): TransactionServiceAsync.WithRawResponse = withRawResponse
+
+    override fun withOptions(modifier: Consumer<ClientOptions.Builder>): TransactionServiceAsync =
+        TransactionServiceAsyncImpl(clientOptions.toBuilder().apply(modifier::accept).build())
+
+    override fun retrieve(
+        params: TransactionRetrieveParams,
+        requestOptions: RequestOptions,
+    ): CompletableFuture<TransactionRetrieveResponse> =
+        // get /api/v1/transactions/{id}
+        withRawResponse().retrieve(params, requestOptions).thenApply { it.parse() }
+
+    override fun list(
+        params: TransactionListParams,
+        requestOptions: RequestOptions,
+    ): CompletableFuture<TransactionListResponse> =
+        // get /api/v1/transactions
+        withRawResponse().list(params, requestOptions).thenApply { it.parse() }
+
+    override fun listByAccount(
+        params: TransactionListByAccountParams,
+        requestOptions: RequestOptions,
+    ): CompletableFuture<List<TransactionListByAccountResponse>> =
+        // get /api/v1/accounts/{account_id}/transactions
+        withRawResponse().listByAccount(params, requestOptions).thenApply { it.parse() }
+
+    class WithRawResponseImpl internal constructor(private val clientOptions: ClientOptions) :
+        TransactionServiceAsync.WithRawResponse {
+
+        private val errorHandler: Handler<HttpResponse> =
+            errorHandler(errorBodyHandler(clientOptions.jsonMapper))
+
+        override fun withOptions(
+            modifier: Consumer<ClientOptions.Builder>
+        ): TransactionServiceAsync.WithRawResponse =
+            TransactionServiceAsyncImpl.WithRawResponseImpl(
+                clientOptions.toBuilder().apply(modifier::accept).build()
+            )
+
+        private val retrieveHandler: Handler<TransactionRetrieveResponse> =
+            jsonHandler<TransactionRetrieveResponse>(clientOptions.jsonMapper)
+
+        override fun retrieve(
+            params: TransactionRetrieveParams,
+            requestOptions: RequestOptions,
+        ): CompletableFuture<HttpResponseFor<TransactionRetrieveResponse>> {
+            // We check here instead of in the params builder because this can be specified
+            // positionally or in the params class.
+            checkRequired("id", params.id().getOrNull())
+            val request =
+                HttpRequest.builder()
+                    .method(HttpMethod.GET)
+                    .baseUrl(clientOptions.baseUrl())
+                    .addPathSegments("api", "v1", "transactions", params._pathParam(0))
+                    .build()
+                    .prepareAsync(clientOptions, params)
+            val requestOptions = requestOptions.applyDefaults(RequestOptions.from(clientOptions))
+            return request
+                .thenComposeAsync { clientOptions.httpClient.executeAsync(it, requestOptions) }
+                .thenApply { response ->
+                    errorHandler.handle(response).parseable {
+                        response
+                            .use { retrieveHandler.handle(it) }
+                            .also {
+                                if (requestOptions.responseValidation!!) {
+                                    it.validate()
+                                }
+                            }
+                    }
+                }
+        }
+
+        private val listHandler: Handler<TransactionListResponse> =
+            jsonHandler<TransactionListResponse>(clientOptions.jsonMapper)
+
+        override fun list(
+            params: TransactionListParams,
+            requestOptions: RequestOptions,
+        ): CompletableFuture<HttpResponseFor<TransactionListResponse>> {
+            val request =
+                HttpRequest.builder()
+                    .method(HttpMethod.GET)
+                    .baseUrl(clientOptions.baseUrl())
+                    .addPathSegments("api", "v1", "transactions")
+                    .build()
+                    .prepareAsync(clientOptions, params)
+            val requestOptions = requestOptions.applyDefaults(RequestOptions.from(clientOptions))
+            return request
+                .thenComposeAsync { clientOptions.httpClient.executeAsync(it, requestOptions) }
+                .thenApply { response ->
+                    errorHandler.handle(response).parseable {
+                        response
+                            .use { listHandler.handle(it) }
+                            .also {
+                                if (requestOptions.responseValidation!!) {
+                                    it.validate()
+                                }
+                            }
+                    }
+                }
+        }
+
+        private val listByAccountHandler: Handler<List<TransactionListByAccountResponse>> =
+            jsonHandler<List<TransactionListByAccountResponse>>(clientOptions.jsonMapper)
+
+        override fun listByAccount(
+            params: TransactionListByAccountParams,
+            requestOptions: RequestOptions,
+        ): CompletableFuture<HttpResponseFor<List<TransactionListByAccountResponse>>> {
+            // We check here instead of in the params builder because this can be specified
+            // positionally or in the params class.
+            checkRequired("accountId", params.accountId().getOrNull())
+            val request =
+                HttpRequest.builder()
+                    .method(HttpMethod.GET)
+                    .baseUrl(clientOptions.baseUrl())
+                    .addPathSegments("api", "v1", "accounts", params._pathParam(0), "transactions")
+                    .build()
+                    .prepareAsync(clientOptions, params)
+            val requestOptions = requestOptions.applyDefaults(RequestOptions.from(clientOptions))
+            return request
+                .thenComposeAsync { clientOptions.httpClient.executeAsync(it, requestOptions) }
+                .thenApply { response ->
+                    errorHandler.handle(response).parseable {
+                        response
+                            .use { listByAccountHandler.handle(it) }
+                            .also {
+                                if (requestOptions.responseValidation!!) {
+                                    it.forEach { it.validate() }
+                                }
+                            }
+                    }
+                }
+        }
+    }
+}
